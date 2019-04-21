e341ae52af0dc8217ac6087247d75bd50e58c75c
町田龍浩 <match8969@gmail.com>
2019-04-21 21:35:35 +0900

    Added annotate to models.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id            :integer          not null, primary key
+#  title         :string
+#  description   :text
+#  goal_price    :integer
+#  current_price :integer
+#  due_date      :datetime
+#  state         :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  user_id       :integer
+#
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
