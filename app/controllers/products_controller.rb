29dda8ad2d9baed35e865896dc67566492970d91
町田龍浩 <match8969@gmail.com>
2019-05-07 16:49:17 +0900

    Modified products_controller after reviewed.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,15 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:title, :description, :goal_price, :current_price, :due_date, :state)
+    end
 end
