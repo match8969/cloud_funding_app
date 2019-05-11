a98aa490845de164872e4ad58a9067adf5224e8b
町田龍浩 <match8969@gmail.com>
2019-05-11 09:47:34 +0900

    Fixed bug to restrict to like own product.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,16 @@
 class LikesController < ApplicationController
   def create
-    @like = current_user.likes.create(product_id: params[:product_id])
-    redirect_back(fallback_location: root_path)
+    if Product.find(params[:product_id]).is_owned_by?(current_user.id)
+      redirect_to  root_path, notice:'You cannot いいね to your own product.'
+    else
+      like = current_user.likes.create(product_id: params[:product_id])
+      redirect_to  root_path
+    end
   end
 
   def destroy
-    @like = Like.find_by(product_id: params[:product_id], user_id: current_user.id)
-    @like.destroy
+    like = Like.find_by(product_id: params[:product_id], user_id: current_user.id)
+    like.destroy
     redirect_back(fallback_location: root_path)
   end
 end
