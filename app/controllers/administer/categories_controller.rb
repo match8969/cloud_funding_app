d24dfb4d7fe4a6c771d85a4f6ba99eba6922504f
町田龍浩 <match8969@gmail.com>
2019-05-14 18:24:10 +0900

    Add administer restrict to category pages.

--- app/controllers/administer/categories_controller.rb
+++ app/controllers/administer/categories_controller.rb
@@ -1,5 +1,6 @@
 class Administer::CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :administer_user
 
   def index
     @categories = Category.all
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,7 @@ class ApplicationController < ActionController::Base
       redirect_to(root_url) unless @user == current_user
     end
 
-  
+    def administer_user
+      redirect_to(root_path, notice: "Permission Error.") unless current_user.role_administer?
+  end
 end
