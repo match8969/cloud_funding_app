1c194f01e8f8bbc3fa5bb46a5830bdf0ed0b8a40
町田龍浩 <match8969@gmail.com>
2019-05-28 21:11:24 +0900

    Modify likes request spec #30

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,18 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
 
-  # TODO: path: products/:id/likes/　のpathを設定する
-  #
-  let!(:user) { FactoryBot.create(:user) }
-  #let!(:product){ FactoryBot.create(:product) }
+  let!(:other_user) { FactoryBot.create(:user, :other_user) }
   let!(:like) { FactoryBot.create(:like, :from_other_user) }
-  #let!(:like) { FactoryBot.create(:like) }
-  #let!(:params) { FactoryBot.attributes_for(like) }
-  let!(:params) { FactoryBot.attributes_for(:like, :from_other_user) }
-
-
-  #let!(:params) {  }
-  #let!(:params) { FactoryBot.attributes_for(:product) }
+  let!(:params) { FactoryBot.attributes_for(:like) }
 
   describe "POST #create" do
     context "ログインユーザーではない場合" do
@@ -26,7 +17,7 @@ RSpec.describe "Likes", type: :request do
 
     context "ログインユーザーの場合" do
       before do
-        sign_in user
+        sign_in other_user
       end
 
       it "結果が期待通りであること" do
@@ -40,20 +31,20 @@ RSpec.describe "Likes", type: :request do
   describe "DELETE #destroy" do
     context "ログインユーザーではない場合" do
       it "結果が期待通りであること" do
-        delete product_like_path(like.product_id, like.id), params: {like: params}
+        delete product_like_path(like.product_id, like.id)
         expect(response).to redirect_to new_user_session_path
       end
     end
 
     context "ログインユーザーの場合" do
       before do
-        sign_in user
+        sign_in other_user
       end
 
       it "結果が期待通りであること" do
         puts "like.id=#{like.id}"
-        delete product_like_path(like.product_id, like.id), params: {product_id: like.id}
-        expect(response).to redirect_to likes_path
+        delete product_like_path(like.product_id, like.id)
+        expect(response).to have_http_status(302)
       end
     end
 
