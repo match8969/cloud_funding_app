40f2c13cfda880b569c37ecf4eaa55dc0969a36f
町田龍浩 <match8969@gmail.com>
2019-05-29 17:34:15 +0900

    Add investment service class #32

---
+++ app/services/investment_report_service.rb
@@ -0,0 +1,21 @@
+class InvestmentReportService
+  def initialize(user)
+    @user = user
+  end
+
+  # TODO:　命名 reportよりも明確に。どんなreport??
+  def report
+    # ロジックを記載する
+    # return: investments
+    # args: user, period
+    # TODO: どうやって情報をとる??
+  end
+
+  # test
+  # return: investments
+  # params: 日付
+  def period_report(begin_date, end_date)
+    # TODO
+  end
+
+end
