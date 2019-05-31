08d9b251e9caedf1cfbc146453852878fdf332cb
町田龍浩 <match8969@gmail.com>
2019-05-31 18:24:33 +0900

    :q

--- app/services/product_achieve_service.rb
+++ app/services/product_achieve_service.rb
@@ -5,20 +5,10 @@ class ProductAchieveService
   end
 
   def achieve
-    # 動作確認OK
-    @all_products = Product.includes(:investments)
-    product_ids = []
-    @all_products.each do |product|
-      product_ids.push(product.id) if product.goal_price <= product.investments.where('created_at <= :due_date', due_date: product.due_date ).pluck(:price).sum
-    end
-    puts "----- product_ids=#{product_ids} -----"
+    product_ids = Product.select { |product|
+      product.goal_price <= product.investments.where('created_at <= :due_date', due_date: product.due_date ).pluck(:price).sum
+    }
     @products = Product.where(id: product_ids)
-    return @products
-
-
-    # TODO: Refactoring
-
-
   end
 
 end
