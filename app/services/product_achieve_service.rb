57dda8a4545c55b3709382a308821986d4c24ba3
町田龍浩 <match8969@gmail.com>
2019-06-01 13:44:05 +0900

    Refactor achieve method #32

--- app/services/product_achieve_service.rb
+++ app/services/product_achieve_service.rb
@@ -4,7 +4,7 @@ class ProductAchieveService
   end
 
   def achieve
-    product_ids = Product.select { |product|
+    product_ids = Product.joins(:investments).includes(:user).select { |product|
       product.goal_price <= product.investments.where('created_at <= :due_date', due_date: product.due_date ).pluck(:price).sum
     }
     @products = Product.where(id: product_ids)
