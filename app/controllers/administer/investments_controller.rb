3e94f8e16c2e3dc45a62f66e78866dcf2e253033
町田龍浩 <match8969@gmail.com>
2019-06-01 16:05:29 +0900

    Add default value to investment report service. #32

--- app/controllers/administer/investments_controller.rb
+++ app/controllers/administer/investments_controller.rb
@@ -10,7 +10,6 @@ class Administer::InvestmentsController < ApplicationController
     investment_report_service = InvestmentReportService.new(params[:begin_datetime], params[:end_datetime] )
     @investments = investment_report_service.period_report
                        .where(product_id: current_user.products.map { |product| product.id})
-
     @products = investment_report_service.period_achieve_products
   end
 
--- app/services/investment_report_service.rb
+++ app/services/investment_report_service.rb
@@ -1,9 +1,9 @@
 class InvestmentReportService
   attr_reader :begin_datetime, :end_datetime
 
-  def initialize(begin_datetime, end_datetime)
-    @begin_datetime = begin_datetime
-    @end_datetime = end_datetime
+  def initialize(begin_datetime=nil, end_datetime=nil)
+    @begin_datetime = begin_datetime ||= Time.zone.now-1.month
+    @end_datetime = end_datetime ||= Time.zone.now
   end
 
   def period_report
@@ -16,7 +16,6 @@ class InvestmentReportService
     @current_achive_products = period_invested_products.select { |product|
       product.goal_price <= product.investments.where('created_at <= :due_date', due_date: product.due_date ).pluck(:price).sum
     }
-
   end
 
 end
