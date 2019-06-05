130aab0e69606be45e5ed1c740066d1d6b215178
町田龍浩 <match8969@gmail.com>
2019-06-05 17:17:56 +0900

    Refactor period_investment spec. #32

--- spec/services/investment_report_service_spec.rb
+++ spec/services/investment_report_service_spec.rb
@@ -7,13 +7,17 @@ describe InvestmentReportService do
   let!(:other_product) { FactoryBot.create(:product, :other_product)}
 
   describe '#period_investments' do
-    let!(:investment) { Investment.create(
-        price: 1,
-        product_id: other_product.id,
-        user_id: user.id,
-        created_at: '2019-01-01 00:00:00',
-        updated_at: '2019-01-01 00:00:00'
-    )}
+    let!(:investment) {
+      Timecop.freeze(Time.local(2019, 1, 1)) {
+        Investment.create(
+          price: 1,
+          product_id: other_product.id,
+          user_id: user.id,
+          created_at: '2019-01-01 00:00:00',
+          updated_at: '2019-01-01 00:00:00'
+        )
+      }
+    }
 
     context '範囲が開始未満の場合' do
       let!(:investment_report_service){
