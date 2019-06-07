7d8da8f9eea0f360dcf6a15ec738d14d80947f7e
町田龍浩 <match8969@gmail.com>
2019-06-07 16:02:50 +0900

    Refactor like creation to add after action. #37

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,12 +6,6 @@ class LikesController < ApplicationController
       redirect_back(fallback_location: root_path, notice: 'You cannot add いいね to your own products.')
     else
       like = current_user.likes.create(product_id: params[:product_id])
-      # Mailの送信
-      UserMailer.with(to_user: like.product.user, from_user: like.user, product: like.product)
-          .send_like_notification.deliver_now
-      # 通知履歴の生成
-      notification = like.product.user.notifications
-                         .create(content: "#{current_user.name}さんが#{like.product.title}に「いいね」をしました。")
       redirect_back(fallback_location: root_path)
     end
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -10,8 +10,15 @@
 #
 
 class Like < ApplicationRecord
+  after_create :send_notification_to_owner
   belongs_to :product
   belongs_to :user
   # 1ユーザーに対して1いいねのみ許可
   validates_uniqueness_of :product_id, scope: :user_id
+
+  def send_notification_to_owner
+    UserMailer.with(to_user: self.product.user, from_user: self.user, product: self.product)
+        .send_like_notification.deliver_now
+  end
+
 end
