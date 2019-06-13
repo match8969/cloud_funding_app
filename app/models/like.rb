82207195e72cf4b71305ff80b3590ffe05219748
町田龍浩 <match8969@gmail.com>
2019-06-13 15:38:56 +0900

    Change to after_commit from after_create. #37

--- app/models/like.rb
+++ app/models/like.rb
@@ -10,16 +10,16 @@
 #
 
 class Like < ApplicationRecord
-  after_create :send_notification_to_owner
+  after_commit :send_notification_to_owner, on: [:create]
   belongs_to :product
   belongs_to :user
   # 1ユーザーに対して1いいねのみ許可
   validates_uniqueness_of :product_id, scope: :user_id
 
   def send_notification_to_owner
-
-    unless UserMailer.with(to_user: self.product.user, from_user: self.user, product: self.product)
-            .send_like_notification.deliver_now.nil?
+    deliver_result = UserMailer.with(to_user: self.product.user, from_user: self.user, product: self.product)
+                 .send_like_notification.deliver_now
+    unless deliver_result.nil?
       # メール送信に問題がなければnotificationをデータベースに保存
       self.product.user.notifications.create(content: "#{self.user.name}さんが#{self.product.title}に「いいね」をしました。")
     end
