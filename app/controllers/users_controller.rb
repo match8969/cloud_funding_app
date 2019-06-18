eae3f0b29e0adccf7ae67beb0d8a47d8579c864d
町田龍浩 <match8969@gmail.com>
2019-06-18 11:45:30 +0900

    Refactor views and controllers. #43
    ビューとコントローラーの微調整と改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,8 @@ class UsersController < ApplicationController
   end
   
   def update
-    # TODO: current_user.categoriesの更新処理
     respond_to do |format|
       if current_user.update(user_params)
-      # if @user.update(user_params)
         format.html { redirect_to current_user, notice: 'user was successfully updated.' }
         format.json { render :show, status: :ok, location: current_user }
       else
@@ -24,14 +22,8 @@ class UsersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #   @user = User.find(params[:id])
-  # end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def user_params
-    params.require(:user).permit(category_ids: [])
-  end
+    def user_params
+      params.require(:user).permit(category_ids: [])
+    end
 
 end
--- app/controllers/watchings_controller.rb
+++ app/controllers/watchings_controller.rb
@@ -2,10 +2,8 @@ class WatchingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # TODO: Rafactor
     watchings_category_ids = current_user.watchings.pluck(:category_id)
     watchings_product_ids = ProductCategory.where(category_id: watchings_category_ids).pluck(:product_id)
-
     @products = Product.where(id: watchings_product_ids).page(params[:page]).per(10)
     @categories = Category.where(id: watchings_category_ids)
   end
