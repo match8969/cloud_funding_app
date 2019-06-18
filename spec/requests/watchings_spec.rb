a2e3c5077a38702889ffbb7db7a4f3c8e7b5ffd0
町田龍浩 <match8969@gmail.com>
2019-06-18 11:14:24 +0900

    Add watchings request spec. #43
    watchings のリクエストスペックの追加

---
+++ spec/requests/watchings_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Watchings", type: :request do
+
+  let!(:user) { FactoryBot.create(:user) }
+
+
+  describe "GET #index" do
+    context "ログインユーザーではない場合" do
+      it "結果が期待通りであること" do
+        get watchings_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    context "ログインユーザーの場合" do
+      before do
+        sign_in user
+      end
+
+      it "結果が期待通りであること" do
+        get watchings_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+  end
+
+end
