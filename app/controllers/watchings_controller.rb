6687b2619b6ed74569e2533aea99077c48f37e42
町田龍浩 <match8969@gmail.com>
2019-06-18 12:20:58 +0900

    Refactor scope and logic in controller. #43
    スコープの名称の変更とwatchings#indexのロジックのincludes追加の改善

--- app/controllers/watchings_controller.rb
+++ app/controllers/watchings_controller.rb
@@ -4,7 +4,8 @@ class WatchingsController < ApplicationController
   def index
     watchings_category_ids = current_user.watchings.pluck(:category_id)
     watchings_product_ids = ProductCategory.where(category_id: watchings_category_ids).pluck(:product_id)
-    @products = Product.where(id: watchings_product_ids).page(params[:page]).per(10)
+    @products = Product.includes(:product_category).includes(:categories).where(id: watchings_product_ids)
+                    .new_order.page(params[:page]).per(10)
     @categories = Category.where(id: watchings_category_ids)
   end
 end
